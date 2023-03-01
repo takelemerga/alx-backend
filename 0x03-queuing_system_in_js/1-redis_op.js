a77459b27a2d695448a979b18e71b4a2b227ec8d
tolaa <mgamtaa@gmail.com>
2023-03-01 10:52:31 +0300

    set and get value functions added

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,39 @@
+import redis from 'redis';
+
+//create redis client
+//use default 127.0.0.1 and port 6379 if
+//not specified as parameter of createClient()
+//e.g const client = redis.createClient(6379, '192.168.10.87');
+
+const client = redis.createClient();
+
+//print a message to the console
+//if we are unable to connect to the Redis server
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+//print a message to the console
+//if we can connect to the Redis server
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+	client.get(schoolName, (err, value) => {
+		if (err) {
+			console.log(err);
+			throw err;
+		}
+	console.log(value);
+	});
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
