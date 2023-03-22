420389a44539d402b26227cda6c02d2da1d4f85c
tolaa <mgamtaa@gmail.com>
2023-03-22 07:25:29 +0300

    flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def get_index() -> str:
+    """
+    creates / route and index.html template
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
